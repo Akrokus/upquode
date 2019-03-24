dd7ba1a8a1830e1b0a86d6dc73feab51ee6a1a41
Oleksandr <kyryk.aleks@gmail.com>
2019-03-24 12:30:36 +0200

    init

---
+++ app/js/command.js
@@ -0,0 +1,121 @@
+(function(){
+    document.addEventListener('DOMContentLoaded', function() {
+        menu();
+        topSlider('.top-slider');
+        topSlider('.product_slider');
+        map();
+        valid();
+        nemuScroll();
+       
+
+        var skillsDiv = $('#skill');
+
+        $(window).on('scroll', function(){
+            var winT = $(window).scrollTop(),
+              winH = $(window).height(),
+              skillsT = skillsDiv.offset().top;
+          if(winT + winH  > skillsT){
+              $(".skills").each(function(){
+              $(this).find('.skill-bar').animate({
+                width:$(this).attr('data-percent')
+              },6000);
+            });
+          }
+        });
+        
+    
+        function menu(){
+          var button = document.querySelector(".burger_menu");
+          var menu = document.querySelector(".small_menu nav")
+          button.addEventListener('click', function(){
+              button.classList.toggle("change");
+              if(button.className=="change"){
+                  menu.parentElement.style.width = "30px";
+                  this.style.width="20px";
+              }else{
+                      menu.parentElement.style.width = "auto";
+                  }
+              
+              menu.classList.toggle("close");
+          })
+          
+      }
+      function topSlider(name){
+          $(name).slick({
+              dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        cssEase: 'linear'
+          });
+          
+      }
+      function map(){
+        var mapProp= {
+        center:new google.maps.LatLng(49.8272808,24.0191034),
+        zoom:15,
+        disableDefaultUI: true,
+        scrollwheel: true,
+      };
+      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+      }
+      function valid(){
+         
+              $(".send").click(function (){	  
+        
+        var name = $(".full_name").val().trim();
+        var email = $(".email_input").val().trim();
+        var massege = $(".massege").val().trim();
+        var checkbox = $("#checkbox-input-1").prop('checked');
+        
+       
+        var error_arr = [];
+        if(name.length == 0) error_arr.push('name');
+        if(email.length == 0) error_arr.push('email');
+        if(massege.length == 0) error_arr.push('massege');	  
+        
+        
+        if(error_arr.length > 0 || checkbox==true){
+        alert("Ви не заповнили всі поля поля\n");
+        
+        return false;
+        }else{
+          
+        console.log("Помилок немає");
+        }  	  
+      });
+          
+      }
+      function nemuScroll(){
+          $(".global_menu a, .logo_link").click(function(){
+              var element = $(this).attr("href");
+              var dist = $(element).offset().top-90;
+      
+              $("html,body").animate({"scrollTop":dist},1000);
+      
+              return false;
+          });
+      
+      
+      
+      
+          $(window).scroll(function(){
+              $("section[id]").each(function(){
+                  var id = $(this).attr("id");
+                  if ($(this).offset().top-100<$(window).scrollTop()){
+                      $('.global_menu a[href="#'+id+'"]').addClass("active").parent().siblings().children().removeClass("active");
+                  }
+                  if($(window).scrollTop()<10){
+                      $(".global_menu a").removeClass("active");
+                  }
+              })
+          })
+      }
+      
+      
+
+
+        })
+}());
+
+
---
+++ gulpFile.js
@@ -0,0 +1,96 @@
+var gulp=require('gulp');
+
+var sass=require('gulp-sass');
+var browserSync=require('browser-sync');
+var concat = require('gulp-concat');
+var uglify =require('gulp-uglifyjs');
+var cssnano=require('gulp-cssnano');
+var rename = require('gulp-rename');
+var del=require('del');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('pngquant');
+var cache = require('gulp-cache');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('scss', function(){
+    return gulp.src('app/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 8', 'ie 7'], {cascado:true}))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('scrips', function(){
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js'
+        
+        
+        
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+})
+
+
+
+
+gulp.task('css-libs', ['scss'],function(){
+    return gulp.src(['app/css/libs.css'])
+    .pipe(cssnano())
+    .pipe(rename({suffix:'.min'}))
+    .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('browser-sync', function(){
+    browserSync({
+        server:{
+            baseDir:'app'
+        },
+        notify:false
+
+    })
+});
+
+gulp.task('clean', function(){
+    return del.sync('dist');
+});
+gulp.task('clear', function(){
+    return cache.clearAll();
+
+});
+
+gulp.task('img', function(){
+    return gulp.src('app/img/**/*')
+    .pipe(cache(imagemin({
+        interlaced:true,
+        progressive:true,
+        svgoPlugins:[{removeViewBox: false}],
+        une:[pngquant()]
+    })))
+    .pipe(gulp.dest('dist/img'));
+
+});
+
+gulp.task('watch',['browser-sync','css-libs', 'scrips'], function(){
+    gulp.watch('app/sass/**/*.scss',['scss']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/*.js', browserSync.reload);
+});
+
+gulp.task('build', ['clean','img','scss','scrips'],function(){
+    var buildCss = gulp.src([
+        'app/css/style.css',
+        'app/css/libs.min.css'
+    ])
+    .pipe(gulp.dest('dist/css'));
+    var buildFonts =gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+
+    var buildJs = gulp.src('app/js/**/*')
+    .pipe(gulp.dest('dist/js'));
+
+    var buildHtml= gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
+
+});
